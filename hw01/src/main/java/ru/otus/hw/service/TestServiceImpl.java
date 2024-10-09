b279d502399a9ec59f8f99398027b2ca5d89edb8
Кононович Михаил Александрович <makononovich@dev.vtb.ru>
2024-10-09 11:04:50 +0300

    Учет замечаний ревью

--- hw01/src/main/java/ru/otus/hw/service/TestServiceImpl.java
+++ hw01/src/main/java/ru/otus/hw/service/TestServiceImpl.java
@@ -3,6 +3,11 @@ package ru.otus.hw.service;
 import lombok.RequiredArgsConstructor;
 import lombok.SneakyThrows;
 import ru.otus.hw.dao.QuestionDao;
+import ru.otus.hw.domain.Answer;
+import ru.otus.hw.domain.Question;
+
+import java.util.List;
+import java.util.concurrent.atomic.AtomicInteger;
 
 @RequiredArgsConstructor
 public class TestServiceImpl implements TestService {
@@ -16,8 +21,18 @@ public class TestServiceImpl implements TestService {
   public void executeTest() {
     ioService.printLine("");
     ioService.printFormattedLine("Please answer the questions below");
-    questionDao.findAll().forEach(System.out::println);
-    // Получить вопросы из дао и вывести их с вариантами ответов
+    questionDao.findAll().forEach(this::outQuestion);
     ioService.printFormattedLine("ok");
+  }
+
+  private void outQuestion(Question question) {
+    ioService.printFormattedLine("Question: %s", question.text());
+    AtomicInteger index=new AtomicInteger(1);
+    question.answers().forEach(x -> outAnswer(index.getAndIncrement(), x));
+  }
+
+  private void outAnswer(int index, Answer answer) {
+    ioService.printFormattedLine("\t %d:Answer %s \t[%s]",index, answer.text(),
+        answer.isCorrect() ? "Good" : "wrong");
   }
 }
